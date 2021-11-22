61992fe1e0d3628a31c7d90926cc65c0fe42c5dd
hyacinthe <hyacinthe.menard@sfr.fr>
2021-11-22 10:17:20 +0700

    commit console game

--- GameConsole.py
+++ GameConsole.py
@@ -1,13 +1,29 @@
 import Board
 
-print("hello world")
-Board.ConsoleGame()
+def ConsoleGame() :
+    for i in range(Board.BOARD_COLUMS) :
+        print(i, end=' ')
+    print()
+    print()
+    for row in Board.board :
+        for elmt in row :
+            print(elmt, end=' ')
+        print()
+    print()
+    print()
 
-for i in range(10):
-    Board.play(Board.CURRENT_IS_RED, 2)
-    Board.ConsoleGame()
-    Board.nextPlayer()
-    Board.play(Board.CURRENT_IS_RED, 3)
-    Board.ConsoleGame()
+print("hello world")
+ConsoleGame()
+
+while Board.getBoardStatus() == "ON_PROGRESS":
+    if Board.CURRENT_IS_RED :
+        player = "red"
+    else :
+        player = "yellow"
+    columnToPlay = -1
+    while not Board.canPlay(columnToPlay) :
+        columnToPlay = int(input("Joueur " + player + " : "))
+    Board.play(Board.CURRENT_IS_RED, columnToPlay)
+    ConsoleGame()
     Board.nextPlayer()
 
